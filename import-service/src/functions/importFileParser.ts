8529f4b496996dc62bd8b574ff879135a602cf1e
Nadzeya Ponkratova <Nadzeya_Ponkratova@epam.com>
2021-09-26 11:39:49 +0300

    add parser and test with mocked url

--- import-service/src/functions/importFileParser.ts
+++ import-service/src/functions/importFileParser.ts
@@ -1,5 +1,7 @@
 'use strict'
 
+import csv from 'csv-parser';
+
 const AWS = require('aws-sdk');
 const BUCKET = 'import-service-dev-serverlessdeploymentbucket-1twvr14t8nqk9';
 
@@ -7,8 +9,25 @@ export const importFileParser = async (event) => {
 	const s3 = new AWS.S3({ region: 'eu-west-1' });
 
 	console.log('RECORDS: ', event.Records);
+	const readedStream = [];
 
 	for(const record of event.Records) {
+		s3.getObject({
+			Bucket: BUCKET,
+			Key: record.s3.object.key
+		}).createReadStream()
+			.pipe(csv())
+			.on('data', (chunk) => {
+				console.log(chunk);
+				readedStream.push(chunk);
+			})
+			.on('end', () => {
+				console.log('END OF STREAM READING: ', readedStream);
+			})
+			.on('error', (error) => {
+				console.error('ERROR READ STREAM: ', error);
+			})
+
 		await s3.copyObject({
 			Bucket: BUCKET,
 			CopySource: BUCKET + '/' + record.s3.object.key,
--- import-service/src/functions/importProductsFile.test.js
+++ import-service/src/functions/importProductsFile.test.js
@@ -1,5 +1,6 @@
 import { importProductsFile } from './importProductsFile'
 import { event } from './../assets/mockedQuery';
+import AWSMock from 'aws-sdk-mock';
 
 const BUCKET = 'import-service-dev-serverlessdeploymentbucket-1twvr14t8nqk9';
 
@@ -14,8 +15,23 @@ test('Get signedURL from s3', () => {
 	});
 });
 
-test('Get signedURL from s3', () => {
+test('Get status code 200 if result correct', () => {
 	importProductsFile(event).then(result => {
 		expect(JSON.parse(result.statusCode)).toEqual(200);
 	});
+});
+
+test('Get status code 200 if result correct and signedURL mocked', async () => {
+	AWSMock.mock('S3', 'getSignedUrl', 'https://mocked-url.com');
+
+	const response = await importProductsFile(event);
+	expect(JSON.parse(response.statusCode)).toEqual(200);
+});
+
+test('Get correct mocked-url if result correct', async () => {
+	const MOCKED_URL = 'https://mocked-url.com';
+	AWSMock.mock('S3', 'getSignedUrl', MOCKED_URL);
+
+	const response = await importProductsFile(event);
+	expect(JSON.parse(response.body)).toEqual(MOCKED_URL);
 });
